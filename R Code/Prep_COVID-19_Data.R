f7692ac005366ae1f361bc6fa080b7771f96c50f
Thomas <thomasmmassie@googlemail.com>
2020-03-13 19:22:46 +0100

    New dashboard, new data source (R)

---
+++ R Code/Prep_COVID-19_Data.R
@@ -0,0 +1,33 @@
+# Clear workspace
+rm(list = ls())
+
+
+# Load packages
+library(tidyverse)
+library(readr)
+
+
+# Load data from GitHub.
+dd_org_confirmed = read_csv(url("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")) %>% 
+  select(., -c("Province/State")) %>% 
+  pivot_longer(., cols = -c("Country/Region", "Lat", "Long"), names_to = "Date", values_to = "confirmed")
+
+dd_org_deaths = read_csv(url("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")) %>% 
+  select(., -c("Province/State")) %>% 
+  pivot_longer(., cols = -c("Country/Region", "Lat", "Long"), names_to = "Date", values_to = "deaths") 
+
+dd_org_recovered = read_csv(url("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")) %>% 
+  select(., -c("Province/State")) %>% 
+  pivot_longer(., cols = -c("Country/Region", "Lat", "Long"), names_to = "Date", values_to = "recovered") 
+
+# Build combined dataset.
+dd <- dd_org_confirmed %>% 
+  mutate(., deaths = dd_org_deaths$deaths, recovered = dd_org_recovered$recovered) %>% 
+  mutate(., active = confirmed - deaths - recovered) %>% 
+  pivot_longer(., cols = c(confirmed, deaths, recovered, active), names_to = "Status", values_to = "Cases")
+
+# Export to .csv file.
+write.csv(dd, "/Users/thomasmassie/Library/Mobile Documents/com~apple~CloudDocs/COVID-19/R Code/COVID_Time_series_TMM.csv")
+
+
+
